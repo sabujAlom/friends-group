2ddb4fa5b52b2b83404fb7b0d2e0f70bcb3c7ec1
sabujAlom <sabujalom18@gmail.com>
2026-04-17 18:47:51 +0600

    footer section added

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../../components/homePage/Banner';
 import AllBooks from '../../components/homePage/AllBooks';
+import Footer from '../../components/homePage/Footer'
 
 const Homepage = () => {
     return (
         <div>
             <Banner/>
             <AllBooks/>
+            <Footer/>
+            
         </div>
     );
 };

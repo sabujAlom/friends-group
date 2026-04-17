5d0ae05732a238f182d0b1b1c4378bf17c28d175
sabujAlom <sabujalom18@gmail.com>
2026-04-17 18:13:04 +0600

    banner section added and navbar created

--- src/components/homePage/Banner.jsx
+++ src/components/homePage/Banner.jsx
@@ -5,12 +5,12 @@ const Banner = () => {
     <div className="hero bg-base-200 min-h-[70vh] rounded-2xl my-8 container mx-auto">
       <div className="space-y-3 flex items-center justify-center flex-col">
 
-      <h1>Friends to keep close in your life</h1>
-      <p>
+      <h1 className="text-5xl font-bold ">Friends to keep close in your life</h1>
+      <p className="font-semibold text-center">
         Your personal shelf of meaningful connections. Browse, tend, and nurture
-        the relationships that matter most.
+        the <br /> relationships that matter most.
       </p> <br />
-      <button className="btn btn-primary">Neutral</button>
+      <button className="btn btn-primary font-semibold"> + Add a Friend</button>
       </div>
     </div>
   );
